896667187f99be7441a7855d349b1f3bf0d91514
Will Melbourne <willmelbourne@gmail.com>
2016-01-04 12:44:59 -0800

    add new content type project, expose it to the api and add the custom fields to it's api

--- wp-content/themes/twentyfifteen-child_api/functions.php
+++ wp-content/themes/twentyfifteen-child_api/functions.php
@@ -1,5 +1,35 @@
 <?php
 
+// add new content type project
+function create_post_type() {
+  register_post_type( 'portfolio_project',
+    array(
+      'labels' => array(
+        'name' => __( 'Projects' ),
+        'singular_name' => 'Project',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Project',
+        'all_items'          => 'All Projects',
+        'search_items'       => 'Search Projects',
+        'not_found'          => 'No Projects Found',
+        'not_found_in_trash' => 'No Projects Found in Trash'
+      ),
+      'show_in_nav_menus'   => true,
+      'show_in_menu'        => true,
+      'public' => true,
+      'has_archive' => true,
+      'show_in_admin_bar' => true,
+      'show_in_rest' => true,
+      'show_in_rest'       => true,
+      'rest_base'          => 'projects'
+    )
+  );
+}
+
+add_action( 'init', 'create_post_type' );
+
+
+// intialize new theme as child theme
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -10,7 +40,7 @@ function theme_enqueue_styles() {
 add_action( 'rest_api_init', 'slug_register_api' );
 
 function slug_register_api() {
-    register_api_field( 'post',
+    register_api_field( 'portfolio_project',
         'gallery_set',
         array(
             'get_callback'    => 'slug_get_api_field',
@@ -19,7 +49,7 @@ function slug_register_api() {
         )
     );
 
-    register_api_field( 'post',
+    register_api_field( 'portfolio_project',
         'project_short_description',
         array(
             'get_callback'    => 'slug_get_api_field',
@@ -28,7 +58,7 @@ function slug_register_api() {
         )
     );
 
-    register_api_field( 'post',
+    register_api_field( 'portfolio_project',
         'font_color',
         array(
             'get_callback'    => 'slug_get_api_field',
@@ -40,7 +70,7 @@ function slug_register_api() {
 
 
 /**
- * Get the value of the "starship" field
+ * Get the value of the api field
  *
  * @param array $object Details of current post.
  * @param string $field_name Name of field.
@@ -49,6 +79,8 @@ function slug_register_api() {
  * @return mixed
  */
 function slug_get_api_field( $object, $field_name, $request ) {
-    // return get_post_meta( $object[ 'id' ], $field_name, true );
     return get_field($field_name, $object[ 'id' ]);
 }
+
+
+
