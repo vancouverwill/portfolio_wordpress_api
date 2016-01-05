a9a8b795d8945b253785cb2214181c3d529f4f8e
Will Melbourne <willmelbourne@gmail.com>
2016-01-04 17:20:20 -0800

    add options page to settings

--- wp-content/themes/twentyfifteen-child_api/functions.php
+++ wp-content/themes/twentyfifteen-child_api/functions.php
@@ -84,3 +84,56 @@ function slug_get_api_field( $object, $field_name, $request ) {
 
 
 
+add_action( 'admin_menu', 'wporg_custom_admin_menu' );
+ 
+function wporg_custom_admin_menu() {
+    // add_options_page(
+    //     'My Plugin Title',
+    //     'Portfolio Settings',
+    //     'manage_options',
+    //     'wporg-plugin',
+    //     'wporg_options_page'
+    // );
+    add_options_page('Global Custom Options', 
+                'Global Custom Options', 
+                'manage_options', 
+                'functions',
+                'global_custom_options');
+
+}
+
+function global_custom_options() {
+    ?>
+    <div class="wrap">
+        <h2>Global Custom Options</h2>
+        <form method="post" action="options.php">
+            <?php wp_nonce_field('update-options') ?>
+            <p>
+                <strong>User Name:</strong><br />
+                <input type="text" name="username" size="45" value="<?php echo get_option('username'); ?>" />
+            </p>
+            <p>
+                <strong>github username:</strong><br />
+                <input type="text" name="github_username" size="45" value="<?php echo get_option('github_username'); ?>" />
+            </p>
+            <p>
+                <strong>linkedin username:</strong><br />
+                <input type="text" name="linkedin_username" size="45" value="<?php echo get_option('linkedin_username'); ?>" />
+            </p>
+            <p>
+                <strong>Twitter ID:</strong><br />
+                <input type="text" name="twitterid" size="45" value="<?php echo get_option('twitterid'); ?>" />
+            </p> 
+            <p>
+                <strong>Bio:</strong><br />
+                <textarea name="user_bio" cols="45" ><?php echo get_option('user_bio'); ?></textarea>
+            </p>
+            <p><input type="submit" name="Submit" value="Store Options" /></p>
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="twitterid,username,github_username,linkedin_username,user_bio" />
+        </form>
+    </div>
+    <?php
+}
+
+
